b3dd9d81eda116945be7170a0a9e3dad9e3173ef
Tafu <162276513+mosoto21@users.noreply.github.com>
2026-07-11 20:37:32 +1000

    changed

--- BallGame/BallGame/GameScene.swift
+++ BallGame/BallGame/GameScene.swift
@@ -1607,7 +1607,7 @@ extension MultipeerManager: MCSessionDelegate {
                  fromPeer peerID: MCPeerID, with progress: Progress) {}
     func session(_ session: MCSession,
                  didFinishReceivingResourceWithName resourceName: String,
-                 fromPeer peerID: MCPeerID, at localURL: URL?, error: Error?) {}
+                 fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {}
 }
 
 extension MultipeerManager: MCNearbyServiceAdvertiserDelegate {
